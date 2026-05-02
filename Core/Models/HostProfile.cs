2fb3e5538af2ae81809205c88b7004762743cbe2
kirillsultan Velikiy Pokrovitel dreams, demon killer 666 <pirogovkirill544@mail.ru>
2026-05-02 16:18:13 +0300

    Change field name to host

--- Core/Models/HostProfile.cs
+++ Core/Models/HostProfile.cs
@@ -1,7 +1,7 @@
 namespace Core.Models ;
 public class HostProfile
 {
-    public string Name { get; set; } = string.Empty;
+    public string Host { get; set; } = string.Empty;
     public int Port { get; set; }
     public string Protocol { get; set; } = string.Empty;
     public AuthData AuthData { get; set; } = new AuthData();
