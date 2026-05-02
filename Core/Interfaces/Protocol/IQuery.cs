8b5961c86472bf7754431d5b63e02d44ddc92494
kirillsultan Velikiy Pokrovitel dreams, demon killer 666 <pirogovkirill544@mail.ru>
2026-05-02 16:15:06 +0300

    Change FileItem to Stream. Файлы по ссш могут передаваться большие, стрим нужен, чтобы передавать по кусочкам, встроенная в си шарп штука, пользуемся ей для работы с файлами, нужно по идее функция выкачать полностью файл со стрима

--- Core/Interfaces/Protocol/IQuery.cs
+++ Core/Interfaces/Protocol/IQuery.cs
@@ -5,7 +5,7 @@ namespace Core.Interfaces.Protocol;
 public interface IQuery
 {
     Task<List<FileItem>> GetFiles(string path);
-    Task<FileItem> GetFile(string path);
+    Task<Stream> GetFile(string path);
     Task<List<string>> GetDirectories(string path);
     Task<OperationStatus> ChangeDirectory(string path);
     Task<OperationStatus> ChangeFile(string path);
