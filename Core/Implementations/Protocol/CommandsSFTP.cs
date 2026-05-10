8dbf2541816e1aa3652d4b1ae098d3c67e132468
kirillsultan Velikiy Pokrovitel dreams, demon killer 666 <pirogovkirill544@mail.ru>
2026-05-10 12:45:54 +0300

    make implementation of sftp client

--- Core/Implementations/Protocol/CommandsSFTP.cs
+++ Core/Implementations/Protocol/CommandsSFTP.cs
@@ -1,7 +1,7 @@
 using Core.Interfaces.Protocol;
 using Core.Models;
 
-using Renci.SshNet
+using Renci.SshNet;
 using Renci.SshNet.Sftp;
 
 namespace Core.Implementations.Protocol;
@@ -9,6 +9,8 @@ namespace Core.Implementations.Protocol;
 public class CommandsSftp : IMethod
 {
     private SftpClient? _client;
+    private string _currentDirectory = "/";
+
     public Task<OperationStatus> Connect(HostProfile profile)
     {
         try
@@ -23,14 +25,14 @@ public class CommandsSftp : IMethod
                 Code = 0,
                 Message = "Connected"
             });
-        } catch(Exception e)
+        }
+        catch (Exception e)
         {
             return Task.FromResult(new OperationStatus
-                {
-                    Code = 1,
-                    Message = e.Message +  "Connection failed"
-                }
-            );
+            {
+                Code = 1,
+                Message = e.Message + " Connection failed"
+            });
         }
     }
 
@@ -42,32 +44,33 @@ public class CommandsSftp : IMethod
             {
                 _client.Disconnect();
                 _client.Dispose();
+                _client = null;
             }
 
             return Task.FromResult(new OperationStatus
-                {
-                    Code = 0,
-                    Message = "Disconnected successfully"
-                }
-            );
+            {
+                Code = 0,
+                Message = "Disconnected successfully"
+            });
         }
         catch (Exception e)
         {
             return Task.FromResult(new OperationStatus
-                {
-                    Code = 1,
-                    Message = e.Message + "Disconnection failed"
-                }
-            );
+            {
+                Code = 1,
+                Message = e.Message + " Disconnection failed"
+            });
         }
     }
 
-    public bool IsConnected => _client?.IsConnected  ?? false;
+    public bool IsConnected => _client?.IsConnected ?? false;
+
     public Task<QueryResult<List<FileItem>>> GetFiles(string path)
     {
         try
         {
             var files = _client!.ListDirectory(path)
+                .Where(f => f.Name != "." && f.Name != "..") // в Filezilla точка передается, хз надо ли нам 
                 .Select(file => new FileItem
                 {
                     Name = file.Name,
@@ -76,13 +79,295 @@ public class CommandsSftp : IMethod
                     IsDirectory = file.IsDirectory,
                     Permissions = GetPermissionsString(file.Attributes),
                 }).ToList();
-            return Task.FromResult(files);
-        } catch(Exception e)
+            return Task.FromResult(new QueryResult<List<FileItem>>
+            {
+                Data = files,
+                Status = new OperationStatus { Code = 0, Message = "All files received" }
+            });
+        }
+        catch (Exception e)
         {
-            return Task.FromResult(new List<FileItem>());
+            return Task.FromResult(new QueryResult<List<FileItem>>
+            {
+                Data = null,
+                Status = new OperationStatus { Code = 1, Message = e.Message + " Failed to receive files" }
+            });
         }
     }
-    
+
+    public Task<QueryResult<Stream>> GetFile(string path)
+    {
+        try
+        {
+            var memoryStream = new MemoryStream();
+            _client!.DownloadFile(path, memoryStream);
+            memoryStream.Position = 0;
+
+            return Task.FromResult(new QueryResult<Stream>
+            {
+                Data = memoryStream,
+                Status = new OperationStatus { Code = 0, Message = "File downloaded" }
+            });
+        }
+        catch (Exception e)
+        {
+            return Task.FromResult(new QueryResult<Stream>
+            {
+                Data = null,
+                Status = new OperationStatus { Code = 1, Message = e.Message + " Failed to download file" }
+            });
+        }
+    }
+
+    public Task<QueryResult<List<string>>> GetDirectories(string path)
+    {
+        try
+        {
+            var dirs = _client!.ListDirectory(path)
+                .Where(f => f.IsDirectory && f.Name != "." && f.Name != "..")
+                .Select(f => f.FullName)
+                .ToList();
+
+            return Task.FromResult(new QueryResult<List<string>>
+            {
+                Data = dirs,
+                Status = new OperationStatus { Code = 0, Message = "Directories received" }
+            });
+        }
+        catch (Exception e)
+        {
+            return Task.FromResult(new QueryResult<List<string>>
+            {
+                Data = null,
+                Status = new OperationStatus { Code = 1, Message = e.Message + " Failed to receive directories" }
+            });
+        }
+    }
+
+    public Task<OperationStatus> SaveFile(string remotePath, Stream content)
+    {
+        try
+        {
+            if (content.CanSeek) content.Position = 0;
+            _client!.UploadFile(content, remotePath, true);
+
+            return Task.FromResult(new OperationStatus
+            {
+                Code = 0,
+                Message = "File saved successfully"
+            });
+        }
+        catch (Exception e)
+        {
+            return Task.FromResult(new OperationStatus
+            {
+                Code = 1,
+                Message = e.Message + " Failed to save file"
+            });
+        }
+    }
+
+    public Task<OperationStatus> CreateFile(string remotePath)
+    {
+        try
+        {
+            using var stream = _client!.Create(remotePath);
+            return Task.FromResult(new OperationStatus
+            {
+                Code = 0,
+                Message = "File created"
+            });
+        }
+        catch (Exception e)
+        {
+            return Task.FromResult(new OperationStatus
+            {
+                Code = 1,
+                Message = e.Message + " Failed to create file"
+            });
+        }
+    }
+
+    public Task<OperationStatus> DeleteFile(string remotePath)
+    {
+        try
+        {
+            _client!.DeleteFile(remotePath);
+            return Task.FromResult(new OperationStatus
+            {
+                Code = 0,
+                Message = "File deleted"
+            });
+        }
+        catch (Exception e)
+        {
+            return Task.FromResult(new OperationStatus
+            {
+                Code = 1,
+                Message = e.Message + " Failed to delete file"
+            });
+        }
+    }
+
+    public Task<OperationStatus> RenameFile(string oldName, string newName)
+    {
+        try
+        {
+            _client!.RenameFile(oldName, newName);
+            return Task.FromResult(new OperationStatus
+            {
+                Code = 0,
+                Message = "File renamed"
+            });
+        }
+        catch (Exception e)
+        {
+            return Task.FromResult(new OperationStatus
+            {
+                Code = 1,
+                Message = e.Message + " Failed to rename file"
+            });
+        }
+    }
+
+    public Task<OperationStatus> CreateDir(string remotePath)
+    {
+        try
+        {
+            _client!.CreateDirectory(remotePath);
+            return Task.FromResult(new OperationStatus
+            {
+                Code = 0,
+                Message = "Directory created"
+            });
+        }
+        catch (Exception e)
+        {
+            return Task.FromResult(new OperationStatus
+            {
+                Code = 1,
+                Message = e.Message + " Failed to create directory"
+            });
+        }
+    }
+
+    public Task<OperationStatus> DeleteDir(string remotePath)
+    {
+        try
+        {
+            DeleteDirectoryRecursive(remotePath);
+            return Task.FromResult(new OperationStatus
+            {
+                Code = 0,
+                Message = "Directory deleted"
+            });
+        }
+        catch (Exception e)
+        {
+            return Task.FromResult(new OperationStatus
+            {
+                Code = 1,
+                Message = e.Message + " Failed to delete directory"
+            });
+        }
+    }
+
+    private void DeleteDirectoryRecursive(string path)
+    {
+        foreach (var entry in _client!.ListDirectory(path))
+        {
+            if (entry.Name is "." or "..") continue;
+
+            if (entry.IsDirectory)
+                DeleteDirectoryRecursive(entry.FullName);
+            else
+                _client.DeleteFile(entry.FullName);
+        }
+        _client.DeleteDirectory(path);
+    }
+
+    public Task<OperationStatus> RenameDir(string oldName, string newName)
+    {
+        try
+        {
+            _client!.RenameFile(oldName, newName);
+            return Task.FromResult(new OperationStatus
+            {
+                Code = 0,
+                Message = "Directory renamed"
+            });
+        }
+        catch (Exception e)
+        {
+            return Task.FromResult(new OperationStatus
+            {
+                Code = 1,
+                Message = e.Message + " Failed to rename directory"
+            });
+        }
+    }
+
+    public Task<OperationStatus> ChangeDirectory(string path)
+    {
+        try
+        {
+            _client!.ChangeDirectory(path);
+            _currentDirectory = _client.WorkingDirectory;
+            return Task.FromResult(new OperationStatus
+            {
+                Code = 0,
+                Message = $"Changed directory to {_currentDirectory}"
+            });
+        }
+        catch (Exception e)
+        {
+            return Task.FromResult(new OperationStatus
+            {
+                Code = 1,
+                Message = e.Message + " Failed to change directory"
+            });
+        }
+    }
+
+    public Task<OperationStatus> ChangeFile(string path)
+    {
+        try
+        {
+            if (!_client!.Exists(path))
+            {
+                return Task.FromResult(new OperationStatus
+                {
+                    Code = 1,
+                    Message = "File does not exist"
+                });
+            }
+
+            var attrs = _client.GetAttributes(path);
+            if (attrs.IsDirectory)
+            {
+                return Task.FromResult(new OperationStatus
+                {
+                    Code = 1,
+                    Message = "Path is a directory, not a file"
+                });
+            }
+
+            return Task.FromResult(new OperationStatus
+            {
+                Code = 0,
+                Message = $"File selected: {path}"
+            });
+        }
+        catch (Exception e)
+        {
+            return Task.FromResult(new OperationStatus
+            {
+                Code = 1,
+                Message = e.Message + " Failed to access file"
+            });
+        }
+    }
+
     private static string GetPermissionsString(SftpFileAttributes attrs)
     {
         return ""
