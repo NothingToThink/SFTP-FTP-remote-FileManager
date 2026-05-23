3cc126d6376b3b95cdff623162bfe801f394e98a
kirillsultan Velikiy Pokrovitel dreams, demon killer 666 <pirogovkirill544@mail.ru>
2026-05-23 20:09:54 +0300

    Добавил новый proj с backend, настрил базовый DI, после пуша сережи заинжектим реализации в интерфейсы, но для разработки это не обязательно, пример запроса написал в папке controllers

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,41 +1,19 @@
+using Core.Implementations.Storage;
+using Core.Interfaces.Manager;
+using Core.Interfaces.Storage;
+using Microsoft.AspNetCore.Connections;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+builder.Services.AddControllers();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
-app.UseHttpsRedirection();
+builder.Services.AddSingleton<IProfileStorage, JsonProfileStorage>();
+builder.Services.AddSingleton<IConnectionManager>();
+builder.Services.AddSingleton<IConnectionFactory>();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+var app = builder.Build(); 
