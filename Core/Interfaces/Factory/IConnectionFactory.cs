dcf0eb3e0995dced685d6e085efc5f51010d30c9
Sergey <vaulinsergej914@gmail.com>
2026-05-23 14:08:43 +0300

    tmp commit

---
+++ Core/Interfaces/Factory/IConnectionFactory.cs
@@ -0,0 +1,10 @@
+using Core.Interfaces.Protocol;
+using Core.Models;
+using Core.Models.Credentials;
+
+namespace Core.Interfaces.Factory;
+
+public interface IRouterFactory
+{
+    Connection CreateMethod(HostProfile profile);
+}
